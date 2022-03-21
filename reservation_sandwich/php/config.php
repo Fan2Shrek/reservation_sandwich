52fbfcb18cc4f7767051cbce4bc7c341a045e3a8
Fan2Shrek <74421318+Fan2Shrek@users.noreply.github.com>
2022-03-21 19:37:17 +0100

    Add files via upload

---
+++ reservation_sandwich/php/config.php
@@ -0,0 +1,15 @@
+<?php 
+
+    #Lien de la BDD 
+    $dbHost = "localhost";
+
+    #Nom de la BDD
+    $dbName = "reservesandwich";
+
+    #User login
+    $dbUser = 'root';
+
+    #User password
+    $dbUserPw = "";
+
+?>
